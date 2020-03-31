f1a048eed1f37f4dbbdf7433c107d1236e6cae94
Mark Turner <mturner437@gmail.com>
2020-03-31 11:22:03 +0100

    Added a base html and a couple other skeleton pages

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def home(request):
     response = render(request, 'main/home.html')
     return response
+    
+def about(request):
+    response = render(request, 'main/about.html')
+    return response
+    
+def contact(request):
+    response = render(request, 'main/contact.html')
+    return response
