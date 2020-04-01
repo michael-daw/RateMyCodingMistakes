1dbad02c9806c6402530214c3ee160652fbe0cbf
Mark Turner <mturner437@gmail.com>
2020-04-01 13:03:48 +0100

    added a bunch of other site stubs

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from datetime import datetime
+import enum
 
 class Category(models.Model):
     
@@ -28,19 +30,20 @@ class Post(models.Model):
     body = models.CharField(max_length=TITLE_MAX_LENGTH)
     op = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post_images', blank=True)
-    date = models.datetime(default=datetime.now())
+    date = models.DateTimeField(default=datetime.now)
     rating = models.IntegerField(default=0)
     
     def __str__(self):
         return self.body
+        
 class Comment(models.Model):
-    
+    pass #Add time
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
     #subclass enum for choice of two experience types - student and professional
-    class ExpChoice(Enum):
+    class ExpChoice(enum.Enum):
         STUDENT = 'STU'
         PROFESSIONAL = 'PRO'
     
--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('base/', views.base, name='base'),
+    path('account/', views.account, name='account'),
+    path('account/login/', views.login, name='login'),
+    path('account/signup/', views.register, name='register'),
+    path('hot/', views.hot, name='hot'),
+    path('new/', views.new, name='new'),
+    path('alltime/', views.alltime, name='alltime'),
+    path('categories/', views.categories, name='categories'),
+    path('sitemap/', views.sitemap, name='sitemap'),
 ]
